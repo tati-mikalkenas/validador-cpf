3498689c3eaae0c0401180d354836afc5c567519
Tati Mikalkenas <110622231+tati-mikalkenas@users.noreply.github.com>
2024-01-19 23:04:46 -0300

    Update Main.java

--- validatorCpf/src/Main.java
+++ validatorCpf/src/Main.java
@@ -1,10 +1,7 @@
 import validatorModel.Menu;
 
-
-import java.text.ParseException;
-
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         Menu menu = new Menu();
         menu.Menu();
 
